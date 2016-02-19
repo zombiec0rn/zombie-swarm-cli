0353ac0324639c1b0e22fa957095e7c2658280e6
Asbjørn Enge <asbjorn@hanafjedle.net>
2016-02-19 23:03:44 +0100

    Stuff

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import fs   from 'fs'
-import yaml from 'js-yaml'
+import fs    from 'fs'
+import yaml  from 'js-yaml'
+import _mdns from 'multicast-dns'
 
 try {
     let doc = yaml.safeLoad(fs.readFileSync('./zombie-swarm.yml'))
@@ -7,3 +8,24 @@ try {
 } catch(e) {
     console.log(e)
 }
+
+let mdns = _mdns()
+
+mdns.on('response', (res) => {
+    let swarmAnswers = res.answers.reduce((found, answer) => {
+        if (answer.name == 'zombie-swarm') found = res.answers
+        return found
+    }, null)
+    if (!swarmAnswers) return
+    console.log('answers', swarmAnswers)
+})
+
+let query = () => {
+    mdns.query({
+        questions: [{
+            name : 'zombie-swarm',
+            type : 'A'
+        }]
+    })
+}
+setInterval(query, 3000)
