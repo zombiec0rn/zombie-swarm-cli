3fd925ad4e9240dbea82d50ea10ab8714ada2329
Asbjørn Enge <asbjorn@hanafjedle.net>
2016-03-11 21:40:22 +0100

    Basic plan creation

--- plan.js
+++ plan.js
@@ -3,6 +3,7 @@ import yaml      from 'js-yaml'
 import scheduler from '@zombiec0rn/zombie-scheduler' 
 import cdi       from 'cccf-docker-instructions'
 import assign    from 'object.assign'
+import uniq      from 'lodash.uniq'
 
 let engines = {
   docker: {
@@ -24,12 +25,50 @@ export function formatPlan(plan) {
   return plan
 }
 
-export default function makePlan(nodes, swarm) {
+export default function makePlan(nodes, wanted) {
   let current = getCurrent(nodes)
-  let wanted  = swarm.services 
-  // Do tag placement
-  // * Assert all tags exists - throw if not
-  // * Add tagged to 
 
-  return scheduler.spread(nodes, wanted, current) 
+  let tags = uniq(nodes.reduce((t, n) => {
+    return t.concat(n.tags || [])
+  },[]).concat(wanted.reduce((t,s) => {
+    return t.concat(s.placement || [])
+  },[])))
+
+  let tagmap = tags.reduce((m, t) => {
+    m[t] = {}
+    m[t].nodes = nodes.filter(n => (n.tags || []).indexOf(t) >= 0)
+    m[t].wanted = wanted.filter(s => (s.placement || []).indexOf(t) >= 0)
+    return m
+  },{})
+
+  let wantedmap = wanted.reduce((m, s) => {
+    m[s.id] = s
+    return m
+  },{})
+
+  // TODO: Sometimes we need to place on all tags, or.. ?
+  // Perhaps we need to add another property or expand tag (stick:gateway)
+  // to support both "on one of these" and "on all of these"
+  // Right now I think we only support "on one of these"
+
+  var tagadds = []
+  tags.forEach(t => {
+    let tagplan = scheduler.spread(tagmap[t].nodes, tagmap[t].wanted)
+    tagplan.add.forEach(s => {
+      assign(s, wantedmap[s.id])
+    })
+    tagadds = tagadds.concat(tagplan.add)
+  })
+
+  let wantedids = wanted.map(s => s.id)
+  let tagaddids = tagadds.map(s => s.id) 
+
+  let plan = scheduler.spread(nodes, wanted, current.concat(tagadds))
+  plan.keep = plan.keep.filter(s => {
+    let istagadd = tagaddids.indexOf(s.id) >= 0
+    if (istagadd) plan.add.push(s) // unshift ? make room for tagged services first ??
+    return !istagadd
+  })
+
+  return plan 
 }
--- test/spec.js
+++ test/spec.js
@@ -1,7 +1,8 @@
 import test from 'ava'
 import 'babel-register'
-import zsf from '@zombiec0rn/zombie-service-format'
-import znf from '@zombiec0rn/zombie-node-format'
+import bytes from 'bytes'
+import zsf   from '@zombiec0rn/zombie-service-format'
+import znf   from '@zombiec0rn/zombie-node-format'
 import makePlan from '../plan'
 import * as utils from '../utils'
 
@@ -39,7 +40,12 @@ test('throws for an invalid nodes.json', t => {
 })
 
 test('createPlan', t => {
-  let nodes = znf.random(10) 
+  let nodes = znf.random(10, {
+    memory : bytes('10GB'),
+    cpus : [
+      { speed : 10000 }
+    ]
+  }) 
   let swarm = utils.readSwarmConfig('./zombie-swarm.yml')
   swarm.services = zsf.random(2).map(s => {
     s.memory = '500MB',
@@ -47,7 +53,18 @@ test('createPlan', t => {
     return s
   })
   nodes[0].tags.push("yologateway")
-  swarm.services[0] = ["yologateway"]
-  let plan = makePlan(nodes, swarm)
-  t.true(plan != undefined)
+  swarm.services[0].placement = ["yologateway"]
+  let plan = makePlan(nodes, swarm.services)
+  t.true(plan.add.length == 2)
+  let swarmserviceids = swarm.services.map(s => s.id)
+  let planaddids = plan.add.map(s => s.id)
+  swarmserviceids.forEach(id => {
+    t.true(planaddids.indexOf(id) >= 0)
+  })
+  let taggedservice = plan.add.filter(s => {
+    return s.id == swarm.services[0].id
+  })[0]
+  t.true(taggedservice.host.hostname == nodes[0].hostname)
+  t.true(taggedservice.placement.indexOf('yologateway') >= 0)
+  t.true(taggedservice.host.tags.indexOf('yologateway') >= 0)
 })
