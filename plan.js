190d8f316c90fd171dd4572756158777fa386b85
Asbjørn Enge <asbjorn@hanafjedle.net>
2016-03-20 22:42:22 +0100

    Plan now using fingerprints

--- plan.js
+++ plan.js
@@ -1,11 +1,26 @@
 import fs        from 'fs'
 import yaml      from 'js-yaml'
 import scheduler from '@zombiec0rn/zombie-scheduler' 
+import zdiff     from '@zombiec0rn/zombie-service-diff'
 import assign    from 'object.assign'
 import uniq      from 'lodash.uniq'
 
 function getCurrent(nodes) {
-  return []
+  return nodes.reduce((services, node) => {
+    let nodeServices = node.services.map(s => {
+      s.host = node
+      if (s.env) {
+        s.env.forEach(e => {
+          if (e.indexOf('ZOMBIE_SWARM_FINGERPRINT') == 0) {
+            s.fingerprint = e.split('=')[1]
+          }
+        })
+      }
+      return s
+    })
+    delete node.services
+    return services.concat(nodeServices)
+  }, [])
 }
 
 export default function makePlan(nodes, wanted) {
@@ -43,8 +58,18 @@ export default function makePlan(nodes, wanted) {
     tagadds = tagadds.concat(tagplan.add)
   })
 
+  let currentids = current.map(s => s.id)
+  tagadds = tagadds.filter(s => currentids.indexOf(s.id) < 0)
   let wantedids = wanted.map(s => s.id)
   let tagaddids = tagadds.map(s => s.id) 
+  let wantedfingerprints = wanted.map(s => {
+    let unified = scheduler.unify([s])[0]
+    return { id: s.id, fingerprint: zdiff.fingerprint(unified) }
+  })
+
+  //console.log('WANTED', wanted)
+  //console.log('CURRENT', current.concat(tagadds))
+  //console.log('TAGADDIDS', tagaddids)
 
   let plan = scheduler.spread(nodes, wanted, current.concat(tagadds))
   plan.keep = plan.keep.filter(s => {
@@ -52,6 +77,14 @@ export default function makePlan(nodes, wanted) {
     if (istagadd) plan.add.push(s) // unshift ? make room for tagged services first ??
     return !istagadd
   })
+
+  plan.add.forEach(s => {
+    if (!s.env) s.env = []
+    let fingerprint = wantedfingerprints.filter(fp => fp.id == s.id)[0].fingerprint
+    s.env.push(`ZOMBIE_SWARM_FINGERPRINT=${fingerprint}`)
+  })
+
+  //console.log('PLAN', JSON.stringify(plan, null, 2))
 
   return plan 
 }
