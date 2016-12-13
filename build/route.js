a3e77fdf158c21dde6f9437070532cb81f01acd7
Asbjørn Enge <asbjorn@hanafjedle.net>
2016-12-13 13:03:19 +0100

    New build

--- build/route.js
+++ build/route.js
@@ -12,6 +12,8 @@ var _iproute2 = _interopRequireDefault(_iproute);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+var exec = require('child_process').exec;
+
 var route = {
   type: 'unicast',
   to: '224.0.0.0/4',
@@ -20,6 +22,13 @@ var route = {
 
 function add(args, callback) {
   route.dev = args.interface;
+  if (process.platform == 'darwin') {
+    exec('sudo route -nv add -net ' + route.to + ' -interface ' + route.dev, function (err, stdout, stderr) {
+      if (err) console.log(err);
+      if (typeof callback == 'function') callback();
+    });
+    return;
+  }
   _iproute2.default.route.add(route, function (err) {
     if (err) console.log(err);
     if (typeof callback == 'function') callback();
@@ -28,6 +37,13 @@ function add(args, callback) {
 
 function del(args, callback) {
   route.dev = args.interface;
+  if (process.platform == 'darwin') {
+    exec('sudo route -v delete -inet ' + route.to + ' -interface ' + route.dev, function (err, stdout, stderr) {
+      if (err) console.log(err);
+      if (typeof callback == 'function') callback();
+    });
+    return;
+  }
   _iproute2.default.route.delete(route, function (err) {
     if (err) console.log(err);
     if (typeof callback == 'function') callback();
